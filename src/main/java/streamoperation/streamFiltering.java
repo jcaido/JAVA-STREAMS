3a1287fad58ecb0c7f04078242dc53402859e753
Cristobal Rosa <crisrogar@gmail.com>
2023-11-19 12:08:10 +0100

    stream filtering

---
+++ src/main/java/streamoperation/streamFiltering.java
@@ -0,0 +1,23 @@
+package streamoperation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class streamFiltering {
+
+    public static void main(String[] args) {
+
+        List<String> vegetables = new ArrayList<>();
+        vegetables.add("spinach");
+        vegetables.add("carrots");
+        vegetables.add("onions");
+        vegetables.add("cucumbers");
+        vegetables.add("tomatoes");
+        vegetables.add("potatoes");
+        vegetables.add("green beans");
+        vegetables.add("peas");
+
+        vegetables.stream().filter(element -> element.contains("c")).forEach(element -> System.out.println(element));
+    }
+}
