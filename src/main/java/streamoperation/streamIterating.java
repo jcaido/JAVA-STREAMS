b923d57df12379733120a66af5c06f298b62f48c
Cristobal Rosa <crisrogar@gmail.com>
2023-11-19 11:49:44 +0100

    stream iterating

---
+++ src/main/java/streamoperation/streamIterating.java
@@ -0,0 +1,26 @@
+package streamoperation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class streamIterating {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        // Stream can be a substitute for loops such as for, for-each, while.
+        for (String element: list) {
+            if (element.contains("a"))
+                System.out.println("true");
+        }
+
+        Stream<String> stream = list.stream();
+        boolean isContain = stream.anyMatch(element -> element.contains("a"));
+        System.out.println(isContain);
+    }
+}
