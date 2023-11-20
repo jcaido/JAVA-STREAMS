44e451c8555fbc80d108aafeaf3767f1597bd319
Cristobal Rosa <crisrogar@gmail.com>
2023-11-20 18:26:41 +0100

    stream reduction

---
+++ src/main/java/streamoperation/streamReduction.java
@@ -0,0 +1,16 @@
+package streamoperation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class streamReduction {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+
+        Integer result = list.stream().reduce(23, (a, b) -> a + b);
+        System.out.println(result);
+    }
+}
